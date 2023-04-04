2b86854961dd50f499553b60470ca8f7b3d88deb
Martin Teigen <martin@teigen.biz>
2023-04-05 01:09:40 +0200

    Fix bugs

--- src/components/grids/grid.style.js
+++ src/components/grids/grid.style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { Grid } from "./grid";
 
 const GridStyled = styled(Grid)`
-  height: 100vh;
+  min-height: 100vh;
   display: grid;
   grid-template-rows: auto 1fr auto;
 `;
