b358ad8d8cbfb83f241db24473a8fab94fc2a69a
Martin Teigen <martin@teigen.biz>
2023-04-05 00:59:42 +0200

    Fix margin

--- src/components/containers/pricesPage/section01/pricesSectionOne.style.js
+++ src/components/containers/pricesPage/section01/pricesSectionOne.style.js
@@ -3,6 +3,7 @@ import { PricesSectionOne } from "./pricesSectionOne";
 import { device } from "../../../../sizes/screenSize.style";
 
 const PricesSectionOneStyled = styled(PricesSectionOne)`
+  margin-bottom: 2rem;
   @media screen and ${device.tabletMax} {
     width: 85%;
   }
