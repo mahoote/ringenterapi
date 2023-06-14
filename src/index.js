9f7e1c7eed4dae3ae6d03bbca9800af4f5d30d7d
Martin Teigen <martin@teigen.biz>
2023-06-14 18:44:07 +0200

    Remove strict mode

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ import { FontStyles } from "./components/globals/fonts/fonts.style";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
     <FontStyles />
     <App />
-  </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
